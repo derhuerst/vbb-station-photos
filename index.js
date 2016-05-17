2903f9093486dda04937b6746292844ccf6d81b2
Jannis R <mail@jannisr.de>
2016-05-17 16:21:56 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,16 @@
 'use strict'
 
-// todo
+const path = require('path')
+const list = require('./photos')
+
+const dir = path.join(__dirname, 'data')
+
+const file = (station, line, perspective) => {
+	if (station in list
+	&& line in list[station]
+	&& perspective in list[station][line])
+		return path.join(dir, [station, line, perspective].join('-') + '.jpg')
+	return null
+}
+
+module.exports = Object.assign(file, {list, dir})
