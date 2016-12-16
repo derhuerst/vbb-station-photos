30aa2048ea26c408980bd05cd29c8a13180c3b34
Jannis R <mail@jannisr.de>
2016-12-16 23:32:05 +0100

    check if station ids are valid :white_check_mark:

--- test.js
+++ test.js
@@ -3,6 +3,8 @@
 
 const test   = require('tape')
 const fs     = require('fs')
+const stations = require('vbb-stations')
+
 const photo = require('./index')
 
 test('file exists', (t) => {
@@ -20,4 +22,11 @@ test('returns null for invalid requests', (t) => {
 	t.equal(photo('100000000000', 'U9', 'platform'), null)
 	t.equal(photo('900000009102', 'Z7', 'platform'), null)
 	t.equal(photo('900000009102', 'U9', 'fooooooo'), null)
+})
+
+test('every station id is valid', (t) => {
+	t.plan(Object.keys(photo.list).length)
+
+	for (let id in photo.list)
+		t.ok(stations(id), `${id} doesn't exist`)
 })
