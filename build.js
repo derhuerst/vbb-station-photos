12af2e87ccda5f4881e9ada39541637648149778
Jannis R <mail@jannisr.de>
2018-03-10 02:19:56 +0100

    don't store photos

--- build.js
+++ build.js
@@ -1,63 +1,55 @@
 'use strict'
 
-const queue  = require('queue')
-const path   = require('path')
+const createQueue = require('queue')
 const flickr = require('flickr-photo-url')
 const commons = require('commons-photo-url')
-const got    = require('got')
-const fs     = require('fs')
+const fs = require('fs')
+const path = require('path')
 
-const list   = require('./photos')
+const photos = require('./photos')
 
-
-
-const urls = {
-	flickr: (data) => flickr(data[0], data[1], 'm'),
-	commons: (data) => Promise.resolve(commons(data[0], 500))
-}
-const url = (data) => {
-	data = Array.from(data)
-	const method = data.shift()
-	if (method in urls) return urls[method](data)
-	else throw new Error('unknown method ' + method)
+const getUrl = (link) => {
+	if (link[0] === 'flickr') return flickr(link[1], link[2])
+	if (link[0] === 'commons') return Promise.resolve(commons(link[1]))
+	return Promise.reject(new Error('unknown link type:' + link[0]))
 }
 
-const b = path.join(__dirname, '.', 'data')
-const q = queue()
-q.concurrency = 5
+const urls = {}
+const resolveLink = (station, line, perspective) => {
+	const task = (cb) => {
+		const link = photos[station][line][perspective]
+		getUrl(link)
+		.then((url) => {
+			if (!urls[station]) urls[station] = {}
+			if (!urls[station][line]) urls[station][line] = {}
+			urls[station][line][perspective] = url
+			cb(null)
+		})
+		.catch(cb)
+	}
+	return task
+}
 
-Object.keys(list).forEach((station) => {
-	const lines = list[station]
-	Object.keys(lines).forEach((line) => {
-		const photos = lines[line]
-		Object.keys(photos).forEach((perspective) => q.push((next) => {
+const queue = createQueue({concurrency: 8, autostart: true})
 
-			if (!photos[perspective]) return next(new Error(
-				`Missing ${perspective} photo for ${line} at ${station}`))
-			const file = [station, line, perspective].join('-') + '.jpg'
+for (let station of Object.keys(photos)) {
+	const lines = photos[station]
+	for (let line of Object.keys(lines)) {
+		const perspectives = lines[line]
+		for (let perspective of Object.keys(perspectives)) {
+			if (!perspectives[perspective]) {
+				console.error(`Missing ${perspective} photo for ${line} at ${station}`)
+			} else queue.push(resolveLink(station, line, perspective))
+		}
+	}
+}
 
-			url(photos[perspective]).catch(next)
-			.then((url) => new Promise((yay, nay) => {
-
-				got.stream(url).on('error', (err) => {
-					if (err.statusCode === 404) {
-						next(new Error(`404 at ${station} (${line} ${perspective})`))
-					} else next(err)
-				})
-				.pipe(fs.createWriteStream(path.join(b, file)))
-				.on('error', nay).on('finish', () => yay())
-
-			})).catch(next)
-			.then(() => {
-				console.info(`${photos[perspective].join('/')} -> ${file}`)
-				next()
-			})
-		}))
-	})
+queue.on('error', (err) => {
+	console.error(err)
+	process.exitCode = 1
 })
 
-q.start()
-q.on('error', (err) => {
-	console.error(err.message)
-	q.start()
+queue.once('end', (err) => {
+	if (err) return null
+	process.stdout.write(JSON.stringify(urls) + '\n')
 })
