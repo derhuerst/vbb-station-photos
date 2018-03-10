4c0c5933c5373335351753bfcea781e11553a739
Jannis R <mail@jannisr.de>
2018-03-10 02:49:29 +0100

    urls for all sizes

--- build.js
+++ build.js
@@ -8,21 +8,32 @@ const path = require('path')
 
 const photos = require('./photos')
 
-const getUrl = (link) => {
-	if (link[0] === 'flickr') return flickr(link[1], link[2])
-	if (link[0] === 'commons') return Promise.resolve(commons(link[1]))
-	return Promise.reject(new Error('unknown link type:' + link[0]))
+const showError = (err) => {
+	console.error(err)
+	process.exitCode = 1
 }
 
-const urls = {}
-const resolveLink = (station, line, perspective) => {
+const sizes = ['original', 'large', 'medium', 'small']
+const urls = {original: {}, large: {}, medium: {}, small: {}}
+
+const resolveLink = (station, line, perspective, size) => {
 	const task = (cb) => {
 		const link = photos[station][line][perspective]
-		getUrl(link)
-		.then((url) => {
-			if (!urls[station]) urls[station] = {}
-			if (!urls[station][line]) urls[station][line] = {}
-			urls[station][line][perspective] = url
+
+		let p
+		if (link[0] === 'flickr') {
+			p = flickr(link[1], link[2], flickr.sizes[size])
+		} else if (link[0] === 'commons') {
+			p = Promise.resolve(commons(link[1]), commons.sizes[size])
+		} else return cb(null, new Error('unknown link type:' + link[0]))
+
+		p.then((url) => {
+			let o = urls[size] || (urls[size] = {})
+			o = o[station] || (o[station] = {})
+			o = o[line] || (o[line] = {})
+			o[perspective] = url
+
+			console.error('✔', station, line, perspective, size)
 			cb(null)
 		})
 		.catch(cb)
@@ -39,17 +50,27 @@ for (let station of Object.keys(photos)) {
 		for (let perspective of Object.keys(perspectives)) {
 			if (!perspectives[perspective]) {
 				console.error(`Missing ${perspective} photo for ${line} at ${station}`)
-			} else queue.push(resolveLink(station, line, perspective))
+				continue
+			}
+			for (let size of sizes) {
+				queue.push(resolveLink(station, line, perspective, size))
+			}
 		}
 	}
 }
 
-queue.on('error', (err) => {
-	console.error(err)
-	process.exitCode = 1
-})
+const writeJSON = (name, data, onError) => {
+	fs.writeFile(path.join(__dirname, name), JSON.stringify(data), (err) => {
+		if (err) onError(err)
+	})
+}
 
+queue.on('error', showError)
 queue.once('end', (err) => {
 	if (err) return null
-	process.stdout.write(JSON.stringify(urls) + '\n')
+
+	writeJSON('original.json', urls.original, showError)
+	writeJSON('large.json', urls.large, showError)
+	writeJSON('medium.json', urls.medium, showError)
+	writeJSON('small.json', urls.small, showError)
 })
---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict'
+
+const original = require('./original.json')
+const large = require('./large.json')
+const medium = require('./medium.json')
+const small = require('./small.json')
+
+module.exports = {original, large, medium, small}
