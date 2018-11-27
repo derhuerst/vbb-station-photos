5580534d2f19e87d5b61a488861f3116b3e625b1
Jannis R <mail@jannisr.de>
2018-11-27 18:34:15 +0100

    throttle Flickr URL check :green_heart:

--- build.js
+++ build.js
@@ -3,6 +3,7 @@
 const createQueue = require('queue')
 const flickr = require('flickr-photo-url')
 const commons = require('commons-photo-url')
+const throttle = require('p-throttle');
 const fs = require('fs')
 const path = require('path')
 const fetch = require('node-fetch')
@@ -25,32 +26,18 @@ const checkUrl = (url) => {
 const sizes = ['original', 'large', 'medium', 'small']
 const urls = {original: {}, large: {}, medium: {}, small: {}}
 
-const resolveLink = (station, line, perspective, size) => {
-	const task = (cb) => {
-		const link = photos[station][line][perspective]
+const resolveLink = throttle((station, line, perspective, size) => {
+	const link = photos[station][line][perspective]
 
-		let p
-		if (link[0] === 'flickr') {
-			p = flickr(link[1], link[2], flickr.sizes[size])
-		} else if (link[0] === 'commons') {
-			p = Promise.resolve(commons(link[1]), commons.sizes[size])
-		} else return cb(null, new Error('unknown link type:' + link[0]))
+	let p
+	if (link[0] === 'flickr') {
+		p = flickr(link[1], link[2], flickr.sizes[size])
+	} else if (link[0] === 'commons') {
+		p = Promise.resolve(commons(link[1]), commons.sizes[size])
+	} else return cb(null, new Error('unknown link type:' + link[0]))
 
-		p
-		.then(checkUrl)
-		.then((url) => {
-			let o = urls[size] || (urls[size] = {})
-			o = o[station] || (o[station] = {})
-			o = o[line] || (o[line] = {})
-			o[perspective] = url
-
-			console.error('✔', station, line, perspective, size)
-			cb(null)
-		})
-		.catch(cb)
-	}
-	return task
-}
+	return p.then(checkUrl)
+}, 3, 1000)
 
 const queue = createQueue({concurrency: 8, autostart: true})
 
@@ -64,7 +51,19 @@ for (let station of Object.keys(photos)) {
 				continue
 			}
 			for (let size of sizes) {
-				queue.push(resolveLink(station, line, perspective, size))
+				queue.push((cb) => {
+					resolveLink(station, line, perspective, size)
+					.then((url) => {
+						let o = urls[size] || (urls[size] = {})
+						o = o[station] || (o[station] = {})
+						o = o[line] || (o[line] = {})
+						o[perspective] = url
+
+						console.error('✔', station, line, perspective, size)
+						cb(null)
+					})
+					.catch(cb)
+				})
 			}
 		}
 	}
