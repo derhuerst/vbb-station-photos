974b9a107663708d20e9aaea5482cee162e9f261
Jannis R <mail@jannisr.de>
2019-02-19 19:12:18 +0100

    build: throttle checkUrl
    closes #24, closes #29

--- build.js
+++ build.js
@@ -1,8 +1,8 @@
 'use strict'
 
 const createQueue = require('queue')
-const flickr = require('flickr-photo-url')
-const commons = require('commons-photo-url')
+const _findFlickrUrl = require('flickr-photo-url')
+const commonsUrl = require('commons-photo-url')
 const throttle = require('p-throttle');
 const fs = require('fs')
 const path = require('path')
@@ -15,29 +15,31 @@ const showError = (err) => {
 	process.exitCode = 1
 }
 
-const checkUrl = (url) => {
+const _checkUrl = (url) => {
 	return fetch(url, {redirect: 'follow'})
 	.then((res) => {
 		if (res.ok) return url
 		throw new Error(res.status + ': ' + url)
 	})
 }
+const checkUrl = throttle(_checkUrl, 5, 10 * 1000)
 
 const sizes = ['original', 'large', 'medium', 'small']
 const urls = {original: {}, large: {}, medium: {}, small: {}}
 
-const resolveLink = throttle((station, line, perspective, size) => {
+const findFlickrUrl = throttle(_findFlickrUrl, 5, 10 * 1000)
+const resolveLink = (station, line, perspective, size) => {
 	const link = photos[station][line][perspective]
 
 	let p
 	if (link[0] === 'flickr') {
-		p = flickr(link[1], link[2], flickr.sizes[size])
+		p = findFlickrUrl(link[1], link[2], flickr.sizes[size])
 	} else if (link[0] === 'commons') {
 		p = Promise.resolve(commons(link[1]), commons.sizes[size])
 	} else return cb(null, new Error('unknown link type:' + link[0]))
 
 	return p.then(checkUrl)
-}, 5, 10000)
+}
 
 const queue = createQueue({concurrency: 8, autostart: true})
 
